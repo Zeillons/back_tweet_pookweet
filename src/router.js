d86b50d412255648d157805038df20fc8fe4c9f4
Zeillons <tgc@live.fr>
2019-11-21 14:06:04 +0100

    timeline ok swagger mieu correction de bug get tweet user

--- src/router.js
+++ src/router.js
@@ -21,16 +21,16 @@ exports.router = (
     /**
      * update tweet
      * @route PUT /tweets/{id}
-     * @param {String} id.route.required - user id
+     * @param {String} id.route.required - tweet id
      * @param {String} tweet.form-data - tweet desciption
      * @returns {object} 201 - The modified ID
      * @returns {object} 500 - Internal error
      * @security JWT
      */
     /**
-     * update user profile
+     * update tweet
      * @route DELETE /tweets/{id}
-     * @param {String} id.route.required - user id
+     * @param {String} id.route.required - tweet id
      * @returns {object} 200 - Tweet deleted
      * @returns {object} 500 - Internal error
      * @security JWT
@@ -46,16 +46,83 @@ exports.router = (
         apiRouter.route('/tweets/:id').get(dbTweet.getTweetById)
             .put(dbTweet.updateTweet)
             .delete(dbTweet.deleteTweet);
-        //TODO : route a faire
+        /**
+     *  get tweet of user
+     * @route GET /tweets/users/{id}
+     * @param {String} id.route.required - tweet id
+     * @returns {Object} 200 - A tweet
+     * @returns {Object} 500 - Internal error
+     * @security JWT
+     */
         apiRouter.route('/tweets/users/:id_user').get(dbTweet.getTweetsUser)
+        /**
+     *  get tweet of user from first index to last index
+     * @route GET /tweets/users/{id}/{from}/{to}
+     * @param {String} id.route.required - tweet id
+     * @param {int} from.route.required - index of first tweet (offset)
+     * @param {int} to.route.required - index of last tweet
+     * @returns {Object} 200 - A list of tweets ids and dates
+     * @returns {Object} 500 - Internal error
+     * @security JWT
+     */
         apiRouter.route('/tweets/users/:id_user/:from/:to').get(dbTweet.getTweetsUserFromXToY)
-
+/**
+     *  get tweet's comment
+     * @route GET /tweets/{id}/comments
+     * @param {String} id.route.required - tweet id
+     * @returns {Object} 200 - A list of tweet's comments
+     * @returns {Object} 500 - Internal error
+     * @security JWT
+     */
+        /**
+     * post a comment
+     * @route POST /tweets/{id}/comments
+     * @param {String} id.route.required - tweet id
+     * @param {String} comment.form-data - comment desciption
+     * @returns {object} 201 - The inserted ID
+     * @returns {object} 500 - Internal error
+     * @security JWT
+     */
         apiRouter.route('/tweets/:id/comments').get(dbComment.getCommentsOfTweet)
             .post(dbComment.createCommentForTweet);
+              /**
+     * update tweet
+     * @route PUT /comments/{id}
+     * @param {String} id.route.required - comment id
+     * @param {String} comment.form-data - comment desciption
+     * @returns {object} 201 - The modified ID
+     * @returns {object} 500 - Internal error
+     * @security JWT
+     */
+    /**
+     * update comments
+     * @route DELETE /comments/{id}
+     * @param {String} id.route.required - comment id
+     * @returns {object} 200 - comment deleted
+     * @returns {object} 500 - Internal error
+     * @security JWT
+     */
+    /**
+     *  get comments
+     * @route GET /comments/{id}
+     * @param {String} id.route.required - comment id
+     * @returns {Object} 200 - A comment
+     * @returns {Object} 500 - Internal error
+     * @security JWT
+     */
         apiRouter.route('/comments/:id').get(dbComment.getCommentById)
             .put(dbComment.updateComment)
             .delete(dbComment.deleteComment);
 
+         /**
+     *  get tweets of a timeline (uses follows)
+     * @route GET /timeline/{from}/{to}
+     * @param {int} from.route.required - index of first tweet (offset)
+     * @param {int} to.route.required - index of last tweet
+     * @returns {Object} 200 - A list of tweets ids and dates
+     * @returns {Object} 500 - Internal error
+     * @security JWT
+     */
         apiRouter.route('/timeline/:from/:to').get(dbTimeline.getTimelineTweetIdFromXToY);
 
         return apiRouter;
--- src/server.js
+++ src/server.js
@@ -12,9 +12,11 @@ const request = require('request');
 const boarder = require('body-parser');
 const version = 'v1';
 const swagger_gen = require('./config/swagger');
+var dev = false
 
 if (process.env.NODE_ENV === 'development') {
-    swagger_gen.swagger(app);
+  swagger_gen.swagger(app);
+  dev = true;
 }
 ////////////////////////
 
@@ -30,42 +32,44 @@ const keycloakHost = process.env.KEYCLOAK_HOST;
 const keycloakPort = process.env.KEYCLOAK_PORT;
 const realmName = process.env.KEYCLOAK_REALM;
 
-// check each request for a valid bearer token
-app.use((req, res, next) => {
-  // assumes bearer token is passed as an authorization header
-  if (req.headers.authorization) {
-    // configure the request to your keycloak server
-    const options = {
-      method: 'GET',
-      url: `http://${keycloakHost}:${keycloakPort}/auth/realms/${realmName}/protocol/openid-connect/userinfo`,
-      headers: {
-        // add the token you received to the userinfo request, sent to keycloak
-        Authorization: req.headers.authorization
-      }
-    };
+if (!dev) {
+  // check each request for a valid bearer token
+  app.use((req, res, next) => {
+    // assumes bearer token is passed as an authorization header
+    if (req.headers.authorization) {
+      // configure the request to your keycloak server
+      const options = {
+        method: 'GET',
+        url: `http://${keycloakHost}:${keycloakPort}/auth/realms/${realmName}/protocol/openid-connect/userinfo`,
+        headers: {
+          // add the token you received to the userinfo request, sent to keycloak
+          Authorization: req.headers.authorization
+        }
+      };
 
-    // send a request to the userinfo endpoint on keycloak
-    request(options, (error, response, body) => {
-      if (error) throw new Error(error);
+      // send a request to the userinfo endpoint on keycloak
+      request(options, (error, response, body) => {
+        if (error) throw new Error(error);
 
-      // if the request status isn't "OK", the token is invalid
-      if (response.statusCode !== 200) {
-        res.status(401).json({
-          error: `unauthorized`
-        });
-      }
-      // the token is valid pass request onto your next function
-      else {
-        next();
-      }
-    });
-  } else {
-    // there is no token, don't process request further
-    res.status(401).json({
-      error: `unauthorized`
-    });
-  }
-});
+        // if the request status isn't "OK", the token is invalid
+        if (response.statusCode !== 200) {
+          res.status(401).json({
+            error: `unauthorized`
+          });
+        }
+        // the token is valid pass request onto your next function
+        else {
+          next();
+        }
+      });
+    } else {
+      // there is no token, don't process request further
+      res.status(401).json({
+        error: `unauthorized`
+      });
+    }
+  });
+}
 
 app.use(
   boarder.urlencoded({
