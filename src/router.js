3485e938a3c892723db49651c392973b85758257
Zeillons <tgc@live.fr>
2019-11-21 14:36:53 +0100

    timeline route is now post

--- src/router.js
+++ src/router.js
@@ -116,14 +116,14 @@ exports.router = (
 
          /**
      *  get tweets of a timeline (uses follows)
-     * @route GET /timeline/{from}/{to}
+     * @route POST /timeline/{from}/{to}
      * @param {int} from.route.required - index of first tweet (offset)
      * @param {int} to.route.required - index of last tweet
      * @returns {Object} 200 - A list of tweets ids and dates
      * @returns {Object} 500 - Internal error
      * @security JWT
      */
-        apiRouter.route('/timeline/:from/:to').get(dbTimeline.getTimelineTweetIdFromXToY);
+        apiRouter.route('/timeline/:from/:to').post(dbTimeline.getTimelineTweetIdFromXToY);
 
         return apiRouter;
     });
